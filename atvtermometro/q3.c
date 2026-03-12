e64941afbd781b3177006a55ad0489395d5d3ae8
sofiarezende <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>
2026-03-12 17:13:06 -0300

    adicionando questão 3

--- atvtermometro/q3.c
+++ atvtermometro/q3.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 int main() {
-    //
-    
-   
+    //soma de numeros em um array
+    int n1, n2, n3, n4, n5, s;
+
+    printf("digite 5 numeros inteiros: \n");
+    scanf("%d \n %d \n %d \n %d \n %d", &n1, &n2, &n3, &n4, &n5);
+
+    s = n1 + n2 + n3 + n4 + n5;
+
+    printf("a soma total e: %d", s);
+
 
     return 0;
 }
